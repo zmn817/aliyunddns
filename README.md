3efe1e43b63b5531fcc2614330e269637a0f76eb
张大蛤 <87500127@qq.com>
2019-04-13 23:12:30 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,4 @@
+阿里云动态解析（DDNS）
+
+1. 添加RAM子账号（https://ram.console.aliyun.com/users）
+2. 设置权限：AliyunHTTPDNSFullAccess(管理HTTPDNS的权限)与AliyunHTTPDNSFullAccess(管理云解析(DNS)的权限)
---
+++ main.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import os
+import json
+import urllib.request
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
+from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+
+
+domain = "daha.wiki"
+rr = "nas"
+client = AcsClient('LTAIN5fW02KpkQNr', 'mFhCLjsVbzpqNQGuAYBN6Qe8JLVhCH', 'cn-hangzhou')
+
+
+# python2:  print(response)
+
+# 获取域名RecordId
+def getRecord():
+    request = DescribeDomainRecordsRequest()
+    request.set_accept_format('json')
+
+    request.set_DomainName(domain)
+
+    response = client.do_action_with_exception(request)
+    response = json.loads(response.decode("UTF-8"))
+    for item in response["DomainRecords"]["Record"]:
+        if item['RR'] == rr:
+            return item
+
+
+def getIP():
+    ip = json.load(urllib.request.urlopen('http://jsonip.com'))['ip']
+    return ip
+
+
+def Dns():
+    request = UpdateDomainRecordRequest()
+    request.set_accept_format('json')
+
+    print('正在获取RecordId……')
+    record = getRecord()
+    recordId = record['RecordId']
+
+    print("获取成功:%s" % recordId)
+    print("获取本机IP……")
+    ip = getIP()
+    print("获取成功:%s" % ip)
+
+    if ip == record['Value']:
+        return print("IP一致无需更新")
+
+    request.set_Value(ip)
+    request.set_RecordId(recordId)
+    request.set_RR(rr)
+    request.set_Type("A")
+
+    print("正在更新DNS……")
+    response = client.do_action_with_exception(request)
+    return json.loads(response.decode("UTF-8"))
+
+if __name__ == "__main__":
+    try:
+        result = Dns()
+        print(result)
+    except (ServerException, ClientException) as reason:
+        print("失败！原因为")
+        print(reason.get_error_msg())
